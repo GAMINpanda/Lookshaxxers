4b420c0ce296c6b495b073b13a196b284cf17593
aak523 <adam.kassam23@imperial.ac.uk>
2024-02-03 22:31:10 +0000

    Icons in HomePageScreen modified

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.compose.runtime:runtime-livedata:1.6.0")
     implementation("androidx.navigation:navigation-compose:2.7.6")
-
     implementation("com.google.firebase:firebase-database-ktx:20.3.0")
     implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
 }
