8785281f3243fd4e23f8ae4df9d762d50112412d
Lorand Webber <lw2223@ic.ac.uk>
2024-02-04 05:55:16 +0000

    Added username field and save button. Does not work though

--- app/src/main/java/com/example/realstudy/userinterface/SettingsScreen.kt
+++ app/src/main/java/com/example/realstudy/userinterface/SettingsScreen.kt
@@ -6,15 +6,24 @@ import coil.request.ImageRequest
 
 
 import android.graphics.drawable.Icon
+import android.widget.Button
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
@@ -22,6 +31,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.realstudy.User
@@ -31,6 +41,10 @@ fun SettingsScreen(user: User, navController:NavController) {
 
     val imageUrl = user.profile.profilePicture
 
+
+    var name by remember { mutableStateOf(user.profile.displayName) }
+
+    // pfp top half
     Box(modifier = Modifier) {
         AsyncImage(model = imageUrl,
                    contentDescription = "background image",
@@ -64,7 +78,7 @@ fun SettingsScreen(user: User, navController:NavController) {
                 .background(
                     Brush.verticalGradient(
                         colors = listOf(Color.White, Color.Transparent),
-                        startY = Float.POSITIVE_INFINITY,
+                        startY = 50f,
 
                         endY = 0f
                     )
@@ -83,9 +97,13 @@ fun SettingsScreen(user: User, navController:NavController) {
                 .fillMaxWidth()
                 .padding(4.dp),
         ) {
-            IconButton(onClick = { navController.popBackStack() }) {
+            IconButton(onClick = { navController.popBackStack() }) { //back arrow
 
-                Icon(Icons.Default.ArrowBack, contentDescription = "Back", modifier= Modifier.padding(bottom = 1.dp))
+                Icon(
+                    Icons.Default.ArrowBack,
+                    contentDescription = "Back",
+                    modifier = Modifier.padding(bottom = 1.dp)
+                )
             }
 
             Text(
@@ -99,11 +117,40 @@ fun SettingsScreen(user: User, navController:NavController) {
 
         }
 
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Username") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 300.dp)
+        )
 
+        fun submitName() {
+            user.updateName(database, name)
+            navController.popBackStack()   //back arrow
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            // Study session button
+            Button(
+                onClick = { submitName() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp),
+                colors = ButtonDefaults.buttonColors(Color.Black)
+            ) {
+                Text(text = "Save")
+            }
+        }
     }
-
-        // Add more UI components for the study session page
-
-        // For example, a timer, progress indicator, etc.
     }
 }
